8f2c18dad6534c1fafc289a029dd3317a2963dab
Aliya Summers <aliyasummers1@gmail.com>
2019-02-25 11:48:19 -0800

    index js pseudo coded

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+'use strict';
+/*
+Function that accepts the name, email, need
+    form
+Have need function separate from name and email function
+    a contact
+    a need
+Constructor function -- will create new Needs_form object and render new objects to bank page with each submit
+Add small grayed out print asking for specifications
+    var Needs_form = function (needer_name, email, need_input){
+        this.needer_name = needer_name;
+        this.email = email;
+        this.need_input = need_input;
+    }
+add category into constructor function as stretch goal
+submit button event listener
+    addEventListener('click', submit)
+submit button event handler
+    var submit_button_handler = function (){
+        put the code in to make the button work
+    }
 
+link to get submit button to lead you to offers bank page
+STORAGE
+    json localStorage thing, Stringify here, parse on bank page
+Stretch goal: PHP third party email.
+    Stretch goal: https://www.freecontactform.com/email_form.php
+Init function that holds all the form stuff
+
+*/
