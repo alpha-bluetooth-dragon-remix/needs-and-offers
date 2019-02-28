3b73b692f3c1ca6c74ba93a0a339ffae6b59be09
Aliya Summers <aliyasummers1@gmail.com>
2019-02-27 17:50:57 -0800

    button interacting with checkbox

--- javascript/need_bank.js
+++ javascript/need_bank.js
@@ -5,7 +5,6 @@
 
 //Global Variable - stores contents from local Storage
 var needs_list = [];
-var checkbox_el = document.createElement('INPUT');
 //Function -- render a form to the needs list page
 
 
@@ -13,10 +12,7 @@ var render_list = function(){
   //brings the contents in storage and moves to global array
   var storage_stringy_needs = localStorage.getItem('all_needs');
   needs_list = JSON.parse(storage_stringy_needs);
-  console.log('needs list at render_list: ', needs_list);
   var target = document.getElementById('needListItems');
-
-  console.log('stuff in needs list right before outer for loop', needs_list);
   //loops through array record by record and renders content to page
   for (var i = 0; i < needs_list.length; i++) {
     var need_ul_el = document.createElement('ul');
@@ -40,8 +36,10 @@ var render_list = function(){
     need_ul_el.appendChild(need_li_el);
 
     //adds checkbox with each new item
-    checkbox_el = document.createElement('INPUT');
+    var checkbox_el = document.createElement('INPUT');
+    checkbox_el.value = needs_list[i].need_input;
     checkbox_el.setAttribute('type', 'checkbox');
+    checkbox_el.setAttribute('class', 'checkbox');
     need_ul_el.appendChild(checkbox_el);
 
     target.appendChild(need_ul_el);
@@ -50,16 +48,31 @@ var render_list = function(){
 render_list();
 
 
-document.getElementById('fillNeeds').onclick = function_name;
+/*
+var boxes_checked = [];
+when a box is checked, boxes_checked.push(entire checked object)
 
+*/
+//var checkbox_el_listener = document.getElementsByTagName('input');
+//checkbox_el.addEventListener('checked', checkbox_el_listener);
+
+
+document.getElementById('fillNeeds').addEventListener('click', function_name);
+
+//checkbox_el.checked counter
 function function_name(){
+  var check_boxes_els = document.getElementsByClassName('checkbox');
+  console.log('checked: ', check_boxes_els);
 // function that grabs checks
-// when render_list item.checked = true
-  if(checkbox_el.checked == true){
-    alert('you have checked a box');
-  }
+// when render_list item.checked === true, it only prints when every checkbox is true
+//for (var j = 0; j < checkbox_el.length; j++){
+//   if(checkbox_el.checked === true){
+//     alert('you have checked a box');
+//   } else{
+//     alert('you did this wrong');
+//   }
+// }
 }
-
 /*
 document.getElementById('fillNeeds').onclick = render_test;
 
